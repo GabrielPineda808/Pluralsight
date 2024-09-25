dba293c36cb0222a6a17b985c83db32b29a104d6
Gabe <gabepineda6@gmail.com>
2024-09-25 10:37:09 -0700

    Pushing local repo to Github

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args){
+        System.out.println("Apples, Bananas, Cereal, Napkins, Bread, Soap, Detergent, Trashbags, etc.");
+    }
+
+}
